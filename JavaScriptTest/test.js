176c82b956bc5d24341e1fccedb165e5af863a59
Jonas Laid <jonas.laid@bellevuecollege.edu>
2018-10-08 12:20:50 -0700

    initial commit

---
+++ JavaScriptTest/test.js
@@ -0,0 +1,10 @@
+var greeting = 'Welcome to Programming 109';
+
+var a = 10;
+var b = 15;
+var surprise = 'Good evening Supreme Leader!!!!!'
+
+console.log(greeting);
+console.log('a + b = ', a + b);
+console.log('a x b = ', a * b);
+window.alert(surprise)
