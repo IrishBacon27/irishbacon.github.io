a8659ced0744be8efc511cacbc071a7396e130b0
Jonas Laid <43526328+IrishBacon27@users.noreply.github.com>
2018-10-15 22:17:26 -0700

    Update index.js

--- ListButton/index.js
+++ ListButton/index.js
@@ -3,7 +3,13 @@ window.onload = () => {
     const setTextButton = document.getElementById('set-text');
     const listButton = document.getElementById('add-to-list');    
     
+  
     setTextButton.onclick = () => {
         userInput.value = 'The first item for my list';
+    }
+
+    listButton.onclick = () => {
+        const myList = document.getElementById('my-list');
+        elfCode.appendToList(myList, userInput.value);
     }
 }
