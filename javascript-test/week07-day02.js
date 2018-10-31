bcd54e3a1b64801f515a6af1c1ecd3c601d908bc
Jonas Laid <jonas.laid@bellevuecollege.edu>
2018-10-31 11:37:24 -0700

    another commit 10/31

--- javascript-test/week07-day02.js
+++ javascript-test/week07-day02.js
@@ -1,5 +1,19 @@
+function message() {
+    console.log("==================");
+    console.log("== " + text);
+    console.log("==================");
+}
+
+message('Operator Precedence');
+
 let result = 1 + (2 * 3);
 console.log(result)
 
-result = (1+2) * 3;
+result = (1 + 2) * 3;
 console.log(result);
+
+message('For Loops');
+
+for (let i = 0; i < 5; i ++) {
+    console.log(i);
+}
