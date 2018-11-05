51b10917b5d8c23797fcd7bddfad824d6d154a21
Jonas Laid <jonas.laid@bellevuecollege.edu>
2018-11-05 12:17:58 -0800

    why

---
+++ javascript-test/function-basics.js
@@ -0,0 +1,20 @@
+function simpleFunction() {
+    console.log("functions should consist of statements designed to perform a single task")
+}
+
+function functionParameters(value) {
+    console.log(value)
+}
+
+function add(a, b) {
+    console.log(a + b);
+}
+
+function adder() {
+    console.log(2 +3);
+}
+
+simpleFunction();
+functionParameters("many functions take parameters");
+add(2, 3);
+adder();
