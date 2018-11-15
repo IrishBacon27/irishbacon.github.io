94c183ed79896b92f2b4dc0adc7fb81bf6359cd7
irishbacon27.github.io <jonaslaid27@gmail.com>
2018-11-15 10:57:36 -0800

    miles

--- javascript-test/function-input.js
+++ javascript-test/function-input.js
@@ -7,7 +7,6 @@ window.onload = () => {
         const feetToInchesDisplay = document.getElementById('feet-to-inches-display');
 
         feetToInchesDisplay.textContent = feetToInches(feetToInchesInput.value);
-    };
 };
 
 const milesToFeetAction =
@@ -20,7 +19,42 @@ milesToFeetAction.onclick = () => {
     milesToFeetDisplay.textContent = milesToFeet(milesToFeetInput.value);
 };
 
+const areaOfTriangleAction = 
+document.getElementById('area-of-triangle-action');
 
+areaOfTriangleAction.onclick = () => {
+    const areaOfTriangleAction = document.getElementById('area-of-triangle-input');
+    const areaOfTriangleDisplay = document.getElementById('are-of-traingle-display');
+
+    areaOfTriangleDisplay.textContent = areaOfTriangle(areaOfTriangleInput.value);
+};
+
+const areaOfCircleAction =
+document.getElementById('area-of-circle-action');
+
+areaOfCircleAction.onclick = () => {
+    const areaOfCircleInput = document.getElementById('area-of-circle-input');
+    const areaOfCircleDisplay = document.getElementById('area-of-circle-display');
+
+    areaOfCircleDisplay.textContent = areaOfCircle(areaOfCircleInput.value)
+};
+};
+
+const feetToInches = (feet) => {
+    return feet * 12;
+}
+
+const milesToFeet = (miles) => {
+    return miles * 5280;
+}
+
+const areaOfTriangle = (base, height) => {
+    return (base * height / 2);
+}
+
+const areaOfCircle = (radius) => {
+    return radius * Math.PI;
+}
 
 
 
