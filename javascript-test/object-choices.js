a542274fed116251e8ced50351ad817d391d5ccc
irishbacon27.github.io <jonaslaid27@gmail.com>
2018-11-19 00:23:23 -0800

    .

--- javascript-test/object-choices.js
+++ javascript-test/object-choices.js
@@ -1,21 +1,9 @@
-window.onload = () => {
-
-    const simpleObject = document.getElementById('function-calls');
-    const callUserInput = document.getElementById('call-user-input')
-
-    functionCalls.onclick = () => {
-        simpleFunction();
-        functionParameters("Many functions take parameters.");
-        functionReturn();
-        const returnValue = functionReturn()
-        console.log(returnValue);
-    }
-//in class work
 function loader () {
-    const objectLiteralButton = document.getElementById('simple-object-action');
-    const functionButton = document.getElementById('getFunctionObject');
+    const simpleObjectButton = document.getElementById('simple-object-action');
+    const functionButton = document.getElementById('function-object-action');
+    const customClassButton = document.getElementById('custom-class-action');
 
-    getNamesButton.onclick = function() {
+    ObjectButton.onclick = function() {
         const name = myObject.getName();
         const getNameDisplay = document.getElementById('getNameDisplay');
         getNameDisplay.textContent = name;
@@ -25,12 +13,11 @@ function loader () {
         getFullNameDisplay.textContent = fullName;
     }
 
-    const functionObject = new FunctionObject();
-    console.log(functionObject.getFullName());
-    //console.log(functionObject.getName());
-    functionButton.onclick = functionObject;
+    const myObject = new FunctionObject();
+    console.log(functionObject.getFullName()); 
 }
 
+window.onload = loader;
 //Key Value pairs
 //Comma separated list of key value pairs
 const myObject = {
@@ -50,10 +37,7 @@ myObject.getFullName = function () {
 }
 
 console.log(myObject.firstName);
-console.log(myObject['firstName']);
-console.log(myObject.getName());
-console.log(myObject.middleName);
-console.log(myObject.getFullName());
+
 
 function FunctionObject() {
     const firstName = 'Jonas';
