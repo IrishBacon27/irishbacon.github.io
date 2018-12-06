164c84a024d7bcb915d1221d013aa4c8bd122306
irishbacon27.github.io <jonaslaid27@gmail.com>
2018-12-05 18:07:13 -0800

    idk anymore

--- javascript-test/radio-choices.js
+++ javascript-test/radio-choices.js
@@ -44,35 +44,3 @@ const setColor = (className) => {
     currentClass = className;
     element.classList.add(className);
 }
-
-// css swap example
-window.onload = () => {
-
-    const appearanceButton = document.getElementById('appearanceAction');
-
-    appearanceButton.onclick = () => {
-        const defaults = document.getElementById('default');
-        const basic = document.getElementById('basic');
-
-        disableStylesheet(defaults);
-        enableStylesheet(basic);
-    }
-    const appearanceButton2 = document.getElementById('defaultAppearanceAction');
-
-    appearanceButton2.onclick = () => {
-        const defaults = document.getElementById('default');
-        const basic = document.getElementById('basic');
-
-        disableStylesheet(basic);
-        enableStylesheet(defaults);
-    }
-
-}
-
-function enableStylesheet (node) {
-    node.rel = 'stylesheet';
-    }
-    
-function disableStylesheet (node) {
-    node.rel = 'alternate stylesheet';
-}     
